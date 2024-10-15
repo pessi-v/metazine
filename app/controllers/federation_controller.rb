8ee87262f85175b6281d7b6064727cb49671866d
pessi-v <pessimo@posteo.net>
2024-10-15 16:17:33 +0200

    add a test Note to prove the outbox concept in mastodon

--- app/controllers/federation_controller.rb
+++ app/controllers/federation_controller.rb
@@ -114,7 +114,24 @@ class FederationController < ApplicationController
           "cc": [],
           "published": Time.current
         },
-        
+        "@context": "https://www.w3.org/ns/activitystreams",
+        "type": "Create",
+        "id": "https://newfutu.re/reader/707",
+        "actor": "https://newfutu.re/@editor",
+        "published": Time.current,
+        "object": {
+          "@context": "https://www.w3.org/ns/activitystreams",
+          "id": "707",
+          "type": "Note",
+          "content": "Testing the fedi things",
+          "url": "https://newfutu.re/reader/707",
+          "attributedTo": "https://newfutu.re/@editor",
+          "to": [
+            "https://www.w3.org/ns/activitystreams#Public"
+          ],
+          "cc": [],
+          "published": Time.current
+        },
       ]
     }), content_type: 'application/activity+json'
   end
