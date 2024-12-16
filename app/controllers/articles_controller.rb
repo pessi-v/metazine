a3b4eb2980325b14d23e34ce001660bed6b2b5aa
pessi-v <pessimo@posteo.net>
2024-12-16 20:12:28 +0100

    cleanup articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,5 @@
 class ArticlesController < ApplicationController
 
-  before_action :set_article, only: %i[ show edit update destroy ]
-
   def frontpage
     latest_articles
   end
@@ -14,11 +12,6 @@ class ArticlesController < ApplicationController
   def articles_by_source
     @articles = Article.where(source_name: params[:source_name]).order(published_at: :desc)
     render :list
-  end
-
-  # GET /articles or /articles.json
-  def index
-    @articles = Article.all
   end
 
   def list
@@ -42,60 +35,8 @@ class ArticlesController < ApplicationController
       format.html
       format.json { render json: @article }
     end
-
-    # respond_to :json, :html
   end
-  # GET /articles/1 or /articles/1.json
-  def show
-  end
-
-  # GET /articles/new
-  def new
-    @article = Article.new
-  end
-
-  # GET /articles/1/edit
-  def edit
-  end
-
-  # POST /articles or /articles.json
-  def create
-    @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /articles/1 or /articles/1.json
-  def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /articles/1 or /articles/1.json
-  def destroy
-    @article.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
+  
   private
     def latest_articles
       @articles = Article.order(published_at: :desc).first(30)
@@ -118,11 +59,6 @@ class ArticlesController < ApplicationController
       readability_output = runner.parse response.body
       article.readability_output = readability_output
       article.save
-    end
-
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
