cc19a75c516fbfebb695b56f2cda4d97693a4f07
pessi-v <pessimo@posteo.net>
2024-12-18 00:57:40 +0100

    add sources factory

---
+++ test/factories/sources.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :source do
+    sequence(:name) { |n| "Source #{n}" }
+    url { "https://example.com/feed" }
+
+    trait :active do
+      active { true }
+    end
+
+    trait :inactive do
+      active { false }
+    end
+  end
+end
