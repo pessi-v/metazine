400886a1233d106ef8e2bc686b9502b44b2ad2bc
pessi-v <pessimo@posteo.net>
2024-12-19 21:20:56 +0100

    split off Articles::TextCleaner

---
+++ app/service_objects/articles/text_cleaner.rb
@@ -0,0 +1,85 @@
+module Articles
+  class TextCleaner
+    def initialize(text)
+      @text = text
+    end
+
+    def clean
+      text
+        .force_encoding('utf-8')
+        .then { |t| strip_formatting(t) }
+        .then { |t| remove_special_characters(t) }
+        .then { |t| fix_spacing(t) }
+        .then { |t| handle_ellipsis(t) }
+        .then { |t| clean_head_and_tail(t) }
+        .strip
+    end
+
+    def clean_with_parentheses
+      clean_parentheses(clean)
+    end
+
+    private
+
+    attr_reader :text
+
+    def strip_formatting(text)
+      text = ApplicationController.helpers.strip_links(text)
+      text = text.sanitize
+      text = ApplicationController.helpers.strip_tags(text)
+      CGI.unescapeHTML(text)
+    end
+
+    def remove_special_characters(text)
+      text.delete("\t").delete("\n")
+    end
+
+    def fix_spacing(text)
+      text = text.squeeze(' ').squeeze('*')
+      text = text.gsub(/\&nbsp;/, " ")
+      text.gsub(/([,\.!?:;])(\S)/, '\1 \2')
+    end
+
+    def handle_ellipsis(text)
+      return remove_last_sentence(text) if text.end_with?('[…]')
+      text
+    end
+
+    def clean_head_and_tail(text)
+      text = remove_head_tag(text)
+      text = handle_tail_ellipsis(text)
+      text = text.capitalize if text == text.upcase
+      text
+    end
+
+    def remove_head_tag(text)
+      return text unless text.start_with?('[') && text.match?(']')
+      
+      closure = text.index(']')
+      text[(closure + 1)..]
+    end
+
+    def handle_tail_ellipsis(text)
+      if (ellipsis = text.index(/(\[…\]){1}/))
+        "#{text[0..ellipsis - 1].strip}…"
+      elsif (ellipsis = text.index(/…{1}/))
+        text[0..ellipsis]
+      else
+        text
+      end
+    end
+
+    def clean_parentheses(text)
+      return text unless (open_parenthesis = text.rindex('('))
+      return text unless (close_parenthesis = text[open_parenthesis..].index(')'))
+
+      cleaned_text = text[0...open_parenthesis] + text[open_parenthesis + close_parenthesis + 1..]
+      clean_parentheses(cleaned_text)
+    end
+
+    def remove_last_sentence(text)
+      last_punctuation_index = text.rindex(/[.!?]/)
+      text[0..last_punctuation_index]
+    end
+  end
+end
