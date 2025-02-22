9d3cfb2dc4319ce20736c374857130527ca61bb3
pessi-v <pessimo@posteo.net>
2025-02-22 16:15:16 +0100

    change approval helper minimum article text limit

--- app/service_objects/articles/approval_helper.rb
+++ app/service_objects/articles/approval_helper.rb
@@ -14,7 +14,7 @@ module Articles
     def approve?
       html_string = @article.readability_output
 
-      if html_string.length < 1400 # filters some cases where no article is shown without Javascript or cookies, and some cases of actually Video/Podcast content
+      if html_string.length < 1900 # filters some cases where no article is shown without Javascript or cookies, and some cases of actually Video/Podcast content
         return false
       elsif FORBIDDEN_STRINGS.any? { |string| html_string.include?(string) }
         return false
