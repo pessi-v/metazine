51421930ae8959fa2ad5cc514dfbe8f44ff2f253
pessi-v <pessimo@posteo.net>
2025-04-26 15:46:58 +0200

    silly me, forgot to uncomment migrations

--- db/migrate/20250425123341_add_federation_to_articles_and_comments.rb
+++ db/migrate/20250425123341_add_federation_to_articles_and_comments.rb
@@ -1,7 +1,6 @@
 class AddFederationToArticlesAndComments < ActiveRecord::Migration[8.0]
   def change
-    # [:articles, :comments].each do |table|
-    [:comments].each do |table|
+    [:articles, :comments].each do |table|
       add_column table, :federated_url, :string, null: true, default: nil
       add_reference table, :federails_actor, null: true, foreign_key: true
     end
--- db/migrate/20250425140024_drop_discussions.rb
+++ db/migrate/20250425140024_drop_discussions.rb
@@ -1,6 +1,6 @@
 class DropDiscussions < ActiveRecord::Migration[8.0]
   def up
-    # drop_table :discussions
+    drop_table :discussions
   end
 
   def down
