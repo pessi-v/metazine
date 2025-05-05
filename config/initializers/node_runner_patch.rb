8cded8649759e86c39a9e0cd7b74b09216445755
pessi-v <pessimo@posteo.net>
2025-05-05 18:18:53 +0200

    try to fix readability

--- config/initializers/node_runner_patch.rb
+++ config/initializers/node_runner_patch.rb
@@ -1,17 +1,17 @@
-module NodeRunnerPatch
-  def create_tempfile(basename)
-    # Force use of Rails tmp directory
-    work_dir = Rails.root.join("tmp", "node_runner_temp")
-    FileUtils.mkdir_p(work_dir) unless File.exist?(work_dir)
+# module NodeRunnerPatch
+#   def create_tempfile(basename)
+#     # Force use of Rails tmp directory
+#     work_dir = Rails.root.join("tmp", "node_runner_temp")
+#     FileUtils.mkdir_p(work_dir) unless File.exist?(work_dir)
 
-    filename = File.join(work_dir, "#{basename[0]}_#{SecureRandom.hex(8)}.#{basename[1]}")
-    File.open(filename, File::WRONLY | File::CREAT | File::EXCL, 0o644)
-  end
+#     filename = File.join(work_dir, "#{basename[0]}_#{SecureRandom.hex(8)}.#{basename[1]}")
+#     File.open(filename, File::WRONLY | File::CREAT | File::EXCL, 0o644)
+#   end
 
-  def write_to_tempfile(contents)
-    tmpfile = create_tempfile(["node_runner", "js"])
-    tmpfile.write(contents)
-    tmpfile.close
-    tmpfile
-  end
-end
+#   def write_to_tempfile(contents)
+#     tmpfile = create_tempfile(["node_runner", "js"])
+#     tmpfile.write(contents)
+#     tmpfile.close
+#     tmpfile
+#   end
+# end
