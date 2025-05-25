2abb66a01289107863f65e98ac268f8d35db5624
pessi-v <pessimo@posteo.net>
2025-05-25 23:40:51 +0200

    creating a comment works

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
   def create
-    Comment.new(content: nil)
+    # binding.break
+    Comment.create(content: params['content'], parent_type: 'Article', parent_id: params['article_id'], federails_actor_id: Current.user.federails_actor.id)
+    redirect_back(fallback_location: frontpage_path)
   end
 
   def update
