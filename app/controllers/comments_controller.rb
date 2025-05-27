ea4fcfc55572f2405408cf028431871ce8c8039f
pessi-v <pessimo@posteo.net>
2025-05-27 14:55:50 +0200

    commenting on comments works; discussion tree in the view works

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,16 @@
 class CommentsController < ApplicationController
   def create
-    article_id = params['article_id']
+    # binding.break
+    # article_id = params["article_id"]
     Comment.create(
-      content: params['content'],
-      parent_type: 'Article',
-      parent_id: article_id,
+      content: params["content"],
+      parent_type: params["parent_type"],
+      parent_id: params["parent_id"],
       federails_actor_id: Current.user.federails_actor.id
     )
-    
-    redirect_to "#{reader_path(article_id)}#discussion"
+
+    # redirect_to "#{reader_path(article_id)}#discussion"
+    redirect_back fallback_location: frontpage_path
   end
 
   def update
