58b30e840e03ea76aed0f30606af685ad2a36c15
Pessi Virta <8080697+pessi-v@users.noreply.github.com>
2026-01-05 19:58:28 +0100

    add some rspec tests (#12)

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,18 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_03_124356) do
   enable_extension "pg_stat_statements"
   enable_extension "unaccent"
 
+  # Custom immutable wrapper around unaccent for use in GIN indexes
+  execute <<-SQL
+    CREATE OR REPLACE FUNCTION public.f_unaccent(input_text text)
+    RETURNS text
+    LANGUAGE plpgsql IMMUTABLE PARALLEL SAFE STRICT AS
+    $$
+    BEGIN
+      RETURN public.unaccent(input_text);
+    END
+    $$;
+  SQL
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "image_url"
