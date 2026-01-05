a58c780509d294e35de6f094df779dfa00eee1cf
pessi-v <pessimo@posteo.net>
2026-01-06 00:59:43 +0100

    add spec for ApprovalHelper

---
+++ spec/service_objects/articles/approval_helper_spec.rb
@@ -0,0 +1,164 @@
+require "rails_helper"
+
+RSpec.describe Articles::ApprovalHelper do
+  let(:source) { create(:source) }
+  let!(:instance_actor) { create(:federails_actor, entity_type: "InstanceActor") }
+
+  describe "#approve?" do
+    context "when readability_output_jsonb is blank" do
+      let(:article) { build(:article, source: source, readability_output_jsonb: nil) }
+
+      it "returns false" do
+        helper = described_class.new(article)
+        expect(helper.approve?).to be false
+      end
+    end
+
+    context "when readability_output_jsonb is an empty hash" do
+      let(:article) { build(:article, source: source, readability_output_jsonb: {}) }
+
+      it "returns false" do
+        helper = described_class.new(article)
+        expect(helper.approve?).to be false
+      end
+    end
+
+    context "when content field is blank" do
+      let(:article) do
+        create(:article, source: source, readability_output_jsonb: {
+          "title" => "Test Article",
+          "content" => nil
+        })
+      end
+
+      it "returns false" do
+        helper = described_class.new(article)
+        expect(helper.approve?).to be false
+      end
+    end
+
+    context "when content field is empty string" do
+      let(:article) do
+        create(:article, source: source, readability_output_jsonb: {
+          "title" => "Test Article",
+          "content" => ""
+        })
+      end
+
+      it "returns false" do
+        helper = described_class.new(article)
+        expect(helper.approve?).to be false
+      end
+    end
+
+    context "when content length is less than 1900" do
+      let(:short_content) { "<p>#{'word ' * 100}</p>" } # Short content
+
+      let(:article) do
+        create(:article, source: source, readability_output_jsonb: {
+          "title" => "Test Article",
+          "content" => short_content,
+          "length" => 500
+        })
+      end
+
+      it "returns false" do
+        helper = described_class.new(article)
+        expect(helper.approve?).to be false
+      end
+    end
+
+    context "when content contains forbidden strings" do
+      let(:article) do
+        create(:article, source: source, readability_output_jsonb: {
+          "title" => "Test Article",
+          "content" => "<p>Of the principles and themes outlined in this issue, Tribune readers will easily discern. \u2018Gastropolitics\u2019 discusses how food matters to socialist politics. Food institutions historic, existing or imagined, are discussed, as well as the transformative urges behind their establishment.</p>",
+          "length" => 2000
+        })
+      end
+
+      it "returns false" do
+        helper = described_class.new(article)
+        expect(helper.approve?).to be false
+      end
+    end
+
+    context "when content is valid and passes all checks" do
+      let(:long_content) { "<p>#{'This is a long article with lots of content. ' * 100}</p>" }
+
+      let(:article) do
+        create(:article, source: source, readability_output_jsonb: {
+          "title" => "Test Article",
+          "content" => long_content,
+          "length" => 5000
+        })
+      end
+
+      it "returns true" do
+        helper = described_class.new(article)
+        expect(helper.approve?).to be true
+      end
+    end
+
+    context "when length field is missing but content is long enough" do
+      let(:long_content) { "<p>#{'This is a long article with lots of content. ' * 100}</p>" }
+
+      let(:article) do
+        create(:article, source: source, readability_output_jsonb: {
+          "title" => "Test Article",
+          "content" => long_content
+        })
+      end
+
+      it "returns true when no length check is applied" do
+        helper = described_class.new(article)
+        expect(helper.approve?).to be true
+      end
+    end
+
+    context "edge case: content length exactly 1899" do
+      let(:article) do
+        create(:article, source: source, readability_output_jsonb: {
+          "title" => "Test Article",
+          "content" => "<p>Some content</p>",
+          "length" => 1899
+        })
+      end
+
+      it "returns false" do
+        helper = described_class.new(article)
+        expect(helper.approve?).to be false
+      end
+    end
+
+    context "edge case: content length exactly 1900" do
+      let(:article) do
+        create(:article, source: source, readability_output_jsonb: {
+          "title" => "Test Article",
+          "content" => "<p>Some content</p>",
+          "length" => 1900
+        })
+      end
+
+      it "returns true" do
+        helper = described_class.new(article)
+        expect(helper.approve?).to be true
+      end
+    end
+
+    context "when content does not contain forbidden strings" do
+      let(:article) do
+        create(:article, source: source, readability_output_jsonb: {
+          "title" => "Test Article",
+          "content" => "<p>This is perfectly normal article content about technology and news.</p>",
+          "length" => 2000
+        })
+      end
+
+      it "returns true" do
+        helper = described_class.new(article)
+        expect(helper.approve?).to be true
+      end
+    end
+  end
+end
