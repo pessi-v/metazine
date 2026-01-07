be07b9472515bfd55cc7afe4f58b4473c729d293
pessi-v <pessimo@posteo.net>
2026-01-07 01:18:51 +0100

    layout fine-tuning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
+
   include Pagy::Backend
 
   helper_method :current_user, :logged_in?, :current_session
