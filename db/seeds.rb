11d329b743b957180c25eebc412d4fdddae2d91c
pessi-v <pessimo@posteo.net>
2026-01-13 15:14:28 +0100

    add sources to seed; fix bugs with feed fetching; add rules for paywalled content

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,5 +15,12 @@ Source.create(name: "The Ecologist", url: "https://theecologist.org/whats_new/fe
 Source.create(name: "Grist", url: "https://grist.org/feed")
 Source.create(name: "e360", url: "https://e360.yale.edu/feed.xml")
 Source.create(name: "London Review of Books", url: "https://www.lrb.co.uk/feeds/rss")
+Source.create(name: "Social Europe", url: "https://www.socialeurope.eu/feed")
+Source.create(name: "n + 1", url: "https://www.nplusonemag.com/feed/")
+Source.create(name: "The Baffler", url: "https://thebaffler.com/homepage/feed")
+Source.create(name: "The Intercept", url: "https://theintercept.com//feed")
+Source.create(name: "Harper's Magazine", url: "https://harpers.org/feed")
+Source.create(name: "Orion", url: "https://orionmagazine.org/article/feed")
+Source.create(name: "The New York Review of Books", url: "https://feeds.feedburner.com/nybooks")
 
 Source.consume_all
